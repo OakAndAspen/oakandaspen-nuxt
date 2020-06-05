d802df6e73c4ac9395b063c504220ae8e56d8943
Irina Despot <irinadespot@gmail.com>
2020-06-05 17:03:59 +0200

    Installed FontAwesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,23 @@ export default {
                     }
                 }
             }
+        ],
+        [
+            'nuxt-fontawesome',
+            {
+                component: 'fa',
+                imports: [
+                    {
+                        set: '@fortawesome/pro-regular-svg-icons',
+                        icons: ['faAngleLeft', 'faAngleRight', 'faBars', 'faBrowser', 'faBullhorn', 'faCode', 'faDice',
+                            'faImage', 'faMusic', 'faPaw', 'faTools']
+                    },
+                    {
+                        set: '@fortawesome/free-brands-svg-icons',
+                        icons: ['faFacebookF', 'faLinkedinIn', 'faGithubAlt']
+                    }
+                ]
+            }
         ]
     ],
     axios: {},
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@
             <h1>This is the Home page</h1>
             <h2>This is a translated message:</h2>
             <p>{{ $t('carousel[0].subtitle') }}</p>
+
+            <p>This is a FontAwesome icon:</p>
+            <fa :icon="['far', 'music']"/>
+            <fa :icon="['fab', 'facebook-f']"/>
         </main>
     </div>
 </template>
